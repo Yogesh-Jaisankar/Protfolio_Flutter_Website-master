5e56ae31677b8dbbc4e54c55a556e39ac79a6a2e
YOGESH JAISANKAR <yogeshjaisnkar@gmail.com>
2023-09-20 13:59:51 +0530

    Portfolio

---
+++ lib/changes/links.dart
@@ -0,0 +1,22 @@
+// CV
+const String resume =
+    'https://drive.google.com/file/d/1bDNNrkQtNVJbXcdwMSx0crQQvAF_nMwV/view?usp=sharing';
+
+// Github Link
+const String gitHub = 'https://github.com/Yogesh-Jaisankar';
+// Github Link
+const String facebook = 'https://www.facebook.com/yogesh.jaisankar.9';
+// Github Link
+const String twitter = 'https://twitter.com/Yogeshtechworks';
+// Github Link
+// const String youtube =
+//     'https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg';
+// Github Link
+const String linkedin = 'https://www.linkedin.com/in/yogeshjaisankar/';
+// Github Link
+const String buymeacoffee = 'https://www.buymeacoffee.com/yogesh05';
+// Github Link
+const String instagram = 'https://www.instagram.com/https.yogesh.io/';
+
+// Github Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=6369686307';
---
+++ lib/changes/strings.dart
@@ -0,0 +1,28 @@
+String hellotag = '''Hello World! ''';
+
+String yourname = "I'm Yogesh Jaisankar,";
+// String hometitle2 = "A Mobile Application\nDeveloper";
+
+String animationtxt1 = " Mobile Application Developer";
+String animationtxt2 = " UI/UX Designer";
+String animationtxt3 = " Web Developer";
+
+//
+String contactHeadding = "Let’s try my service now!";
+String contactSubHeadding =
+    "Let’s work together and make everything super cute and super useful.";
+//
+String miniDescription =
+    "Freelancer providing services for programming and design content needs. Join me down below and let's get started!";
+
+String servicesSubHeading =
+    "Since the beginning of my journey as a freelance designer and developer, I've worked in startups and collaborated with talented people to create digital products for both business and consumer use. I offer a wide range of services, including brand design, programming and teaching.";
+//
+
+String protfolioSubHeading =
+    "Since the beginning of my journey as a designer and developer, I have created digital products for business and consumer use. This is a little bit.";
+const String aboutMeHeadline =
+    'I\'m Yogesh Jaisankar, a Mobile App developer, Web developer and UI designer.';
+
+const String aboutMeDetail =
+    "As an accomplished Mobile App Developer and driven tech entrepreneur, I bring over four years of dedicated experience in crafting innovative mobile applications. Currently pursuing my M.Tech at Vellore Institute of Technology, Chennai, I am passionate about pushing the boundaries of mobile technology to create user-friendly and cutting-edge solutions";
